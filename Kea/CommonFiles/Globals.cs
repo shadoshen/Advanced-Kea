c8702baaaf12a3b2a0b2b7ef30b362142975a855
shadoshen <shadowarrior82@gmail.com>
2026-07-11 14:24:48 +0200

    Add XPath constants and update user agent
    Added new XPath constants for episode list and image extraction, and updated the user agent string.

--- Kea/CommonFiles/Globals.cs
+++ Kea/CommonFiles/Globals.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,16 +10,18 @@ namespace Kea.CommonFiles
 	{
 		//Contains constants & variables that are shared between all classes.
 		public const ushort maxSingleImageHeight = 30000;
-		
-		public const string episodeListHtmlXPath = "//body/div[@id='wrap']/div[@id='container']/div[@id='content']/div[@class='cont_box']/div[starts-with(@class,'detail_body')]/div[@class='detail_lst']";
-		public const string episodeListItemHtmlXPath = episodeListHtmlXPath + "/ul[@id='_listUl']/li[@class='_episodeItem']";
-		public const string episodeListPaginatorXPath = episodeListHtmlXPath + "/div[@class='paginate']/a";
-		
-		public const string episodeImageHtmlXPath = "//body/div[@id='wrap']/div[@id='container']/div[@id='content']/div[@class='cont_box']/div[@class='viewer_lst']/div[@id='_imageList']/img";
+
+        // --- NEW PATHS – ANSWER KEYS ---
+        public const string episodeListHtmlXPath = "//ul[@id='_listUl']";
+        public const string episodeListItemHtmlXPath = "//ul[@id='_listUl']/li[contains(@class, '_episodeItem')]";
+        public const string episodeListPaginatorXPath = "//div[contains(@class, 'paginate')]/a";
+
+        public const string episodeImageHtmlXPath = "//body/div[@id='wrap']/div[@id='container']/div[@id='content']/div[@class='cont_box']/div[@class='viewer_lst']/div[@id='_imageList']/img";
 		
 		public const string naverWebtoonAPIBaseUrl = "https://global.apis.naver.com/lineWebtoon";
-		
-		public const string spoofedUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0";
-		
-	}
+
+        //Brand-new 2026 User-Agent
+        public const string spoofedUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36";
+
+    }
 }
